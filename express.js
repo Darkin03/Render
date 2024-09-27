0b736c8f738ff7d8bfbc94f51e5e3f549a8a5a31
Darkin03 <esnaiderortega@unisinu.edu.co>
2024-09-26 21:18:05 -0500

    Deploy appy

--- express.js
+++ express.js
@@ -1,24 +1,10 @@
 const express = require('express')
-const fs = require('node:fs')
 const app = express()
 app.disable('x-powered-by')
 const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html;charset=utf-8')
-  fs.readFile('./dialogo.txt', 'utf-8', (err, data) => {
-    if (err) {
-      res.statusCode = 500
-      res.end('<h2>No se pudo encontrar los recurso correspondientes</h2>')
-    } else {
-      res.end(`<h2>${data}</h2>`)
-    }
-  })
-})
-
-app.post('/', (req, res) => {
-  res.status(201).send('<h1>Recurso creado</h1>')
+  res.status(201).send('<h1>Nuestra Api fue desplegada!!!</h1>')
 })
 
 app.listen(PORT, () => {
